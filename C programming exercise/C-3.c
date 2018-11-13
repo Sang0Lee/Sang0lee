b2db11a3e73f70187c8ebc3b31a9acf7a87460bd
Sang0Lee <44976020+Sang0Lee@users.noreply.github.com>
2018-11-13 22:31:16 +0900

    exercise-3
    구구단 출력 프로그램을 작성하시오

---
+++ C programming exercise/C-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	int a, b, c;
+
+	for(a=1; a < 10; a++)
+	{
+		for (b=1; b < 10; b++)
+		{
+			c = a*b;
+			printf("%d * %d = %d\n",a,b,c);
+		}
+		printf("\n");
+	}
+
+	while(1);
+}
